8a773424c842614e8de77ca6ae82988f8802f553
Pristine Shin <prisvsyo@gmail.com>
2024-09-23 16:33:47 -0700

    completed track the robot bonus problem

--- Mod 6 Week 1 (31)/5-day-BONUS-track-the-robot-part-time/track-the-robot.py
+++ Mod 6 Week 1 (31)/5-day-BONUS-track-the-robot-part-time/track-the-robot.py
@@ -6,6 +6,25 @@
 # left and 40 up from where it started, so you should return `[-20, 40]`.
 
 # Write your function here.
+def track_robot(instructions):
+    x = 0
+    y = 0
+
+    if len(instructions) == 0:
+        return [x, y]
+
+    for m in instructions:
+        mSplit = m.split(' ')
+        if mSplit[0] == 'right':
+            x += int(mSplit[1])
+        elif mSplit[0] == 'left':
+            x -= int(mSplit[1])
+        elif mSplit[0] == 'up':
+            y += int(mSplit[1])
+        elif mSplit[0] == 'down':
+            y -= int(mSplit[1])
+
+    return [x, y]
 
 print(track_robot(["right 10", "up 50", "left 30", "down 10"]))
 # Prints [-20, 40]
