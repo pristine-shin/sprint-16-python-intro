f69157f68b57749a3fc1d9bee46944b820d42051
Pristine Shin <prisvsyo@gmail.com>
2024-09-26 16:54:42 -0700

    completed class practice besides last challenge problem

--- Mod 6 Week 2 (32)/3-day-basic-class-syntax-part-time/problems/03-regular-polygon.py
+++ Mod 6 Week 2 (32)/3-day-basic-class-syntax-part-time/problems/03-regular-polygon.py
@@ -5,7 +5,7 @@
 #    sides is less than 3, raise an Exception with the message "A polygon must
 #    have at least 3 sides."
 
-# 2. </li><li>A class variable called type set to an inital value of "Polygon"
+# 2. A class variable called type set to an inital value of "Polygon"
 
 # 3. An instance method called identify_polygon that identifies the type of a
 #    polygon based on its number of sides. This method should set the type class
@@ -17,7 +17,7 @@
 #    as an argument, where each tuple contains the (num_sides, length) for each
 #    RegularPolygon instance to be created. The method should return a list of
 #    all the class instances created.
- 
+
 # 5. A static method called get_perimeter that calculates and returns the
 #    perimeter of a polygon. This method should take an instance of the
 #    `RegularPolygon` class and return the calculated perimeter. (The perimeter
@@ -36,7 +36,56 @@
 # Greater than 10 sides - Polygon with n sides
 
 # Write your class here.
+class RegularPolygon:
+    type = "Polygon"
 
+    def __init__(self, num_sides, length):
+        if num_sides < 3:
+            raise Exception("A polygon must have at least 3 sides")
+        self._num_sides = num_sides
+        self._length = length
+
+    @property
+    def num_sides(self):
+        return self._num_sides
+
+    @property
+    def length(self):
+        return self._length
+
+    def identify_polygon(self):
+        if self._num_sides == 3:
+            self.type = "Triangle"
+        elif self._num_sides == 4:
+            self.type = "Quadrilateral"
+        elif self._num_sides == 5:
+            self.type = "Pentagon"
+        elif self._num_sides == 6:
+            self.type = "Hexagon"
+        elif self._num_sides == 7:
+            self.type = "Heptagon"
+        elif self._num_sides == 8:
+            self.type = "Octagon"
+        elif self._num_sides == 9:
+            self.type = "Nonagon"
+        elif self._num_sides == 10:
+            self.type = "Decagon"
+        elif self._num_sides > 10:
+            self.type = f"Polygon with {self._num_sides} sides"
+
+    @classmethod
+    def polygon_factory(cls, lst):
+        result = []
+        for tupl in lst:
+            result.append(cls(tupl[0], tupl[1]))
+        return result
+
+    @staticmethod
+    def get_perimeter(instance):
+        return instance.num_sides * instance.length
+
+    def __repr__(self):
+        return f'<Regular Polygon object with {self._num_sides} sides of length {self._length}>'
 
 pentagon = RegularPolygon(5, 5)
 octagon = RegularPolygon(8, 10)
@@ -54,10 +103,13 @@ print(pentagon.type) # Pentagon
 print(octagon.type) # Octagon
 print(dodecagon.type) # Polygon with 12 sides
 
+print(RegularPolygon.polygon_factory([(5, 5), (3, 2), (8, 10)])) # prints a list of 3 RegularPolygon objects
+
 print(RegularPolygon.get_perimeter(pentagon)) # 25
 print(RegularPolygon.get_perimeter(octagon)) # 80
 print(RegularPolygon.get_perimeter(dodecagon)) # 12
 
-print(RegularPolygon.polygon_factory([(5, 5), (3, 2), (8, 10)])) # prints a list of 3 RegularPolygon objects
-
-not_a_polygon = RegularPolygon(2, 5) # Exception: A polygon must have at least 3 sides.
+try:
+    not_a_polygon = RegularPolygon(2, 5) # Exception: A polygon must have at least 3 sides.
+except Exception as e:
+    print(f'Exception: {e}')
